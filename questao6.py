ab73c45886be91c4934e2cfb212a5734087e0ddc
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-23 15:13:29 -0300

    Atualizar o questao6.py

--- questao6.py
+++ questao6.py
@@ -2,11 +2,11 @@ def main():
   if angulo1 + angulo2 + angulo3 != 180:
     print('NÃO é um Triângulo!')
   elif angulo1 < 90 and angulo2 < 90 and angulo3 < 90:
-    print('O triângulo é Acutângulo!')
+    print('O Triângulo é Acutângulo!')
   elif angulo1 == 90 or angulo2 == 90 or angulo3 == 90:
-     print('O triângulo é Retângulo!')
+     print('O Triângulo é Retângulo!')
   elif angulo1 > 90 or angulo2 > 90 or angulo3 > 90:
-   print('O triângulo é Obtusângulo!')
+   print('O Triângulo é Obtusângulo!')
 
 #--------------------
 
