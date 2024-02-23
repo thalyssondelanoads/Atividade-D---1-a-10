b7f1668b9121bba2b4b7aecaecb2bcaf231c934e
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-22 23:47:33 -0300

    Create questao8.py

---
+++ questao8.py
@@ -0,0 +1,29 @@
+def main_idade():
+  if mes_atual > mes_nasc:
+    print(f'Sua idade é {dif_ano} ')
+  elif mes_atual < mes_nasc:
+    print(f'Sua idade é {dif_ano1}')
+  elif dia_atual > dia_nasc:
+    print(f'Sua idade é {dif_ano}')
+  else:
+    print(f'Sua idade é {dif_ano1}')
+
+#---------------------
+
+print('Sua Idade!')
+print('-----------')
+      
+dia_atual = int(input('Digite o Dia atual: '))
+mes_atual = int(input('Digite o Mês atual: '))
+ano_atual = int(input('Digite o Ano atual: '))
+
+print('-----------')
+dia_nasc = int(input('Digite o Dia que vc nasceu: '))
+mes_nasc = int(input('Digite o Mês que vc nasceu: '))
+ano_nasc = int(input('Digite o Ano que vc nasceu: '))
+
+dif_ano = ano_atual - ano_nasc
+dif_ano1 = (ano_atual - ano_nasc) - 1
+
+print('-----------')
+main_idade()
