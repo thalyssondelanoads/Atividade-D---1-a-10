4c4c27ac633251bb23320182bcb98069acf61cb1
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-22 22:35:59 -0300

    Create questao6.py

---
+++ questao6.py
@@ -0,0 +1,21 @@
+def main():
+  if angulo1 + angulo2 + angulo3 != 180:
+    print('NÃO é um Triângulo!')
+  elif angulo1 < 90 and angulo2 < 90 and angulo3 < 90:
+    print('O triângulo é Acutângulo!')
+  elif angulo1 == 90 or angulo2 == 90 or angulo3 == 90:
+     print('O triângulo é Retângulo!')
+  elif angulo1 > 90 or angulo2 > 90 > angulo3 > 90:
+   print('O triângulo é Obtusângulo!')
+
+#--------------------
+
+print('Indentificar e Classificar Triângulos')
+print('---------')
+
+angulo1 = int(input('Digite o 1º Ângulo: '))
+angulo2 = int(input('Digite o 2º Ângulo: '))
+angulo3 = int(input('Digite o 3º Ângulo: '))
+
+print('---------')
+main()      
