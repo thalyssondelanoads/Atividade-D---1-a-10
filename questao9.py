2ae987f22858e0eab681f9a0b436fea131e9b775
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-26 23:13:42 -0300

    Update questao9.py

--- questao9.py
+++ questao9.py
@@ -1,1 +1,29 @@
-.
+def num_primo():
+    if num % 2 == 0:
+        print('O Número NÃO é Primo')
+    elif num % 3 == 0:
+        print('O Número NÃO é Primo')
+    elif num % 4 == 0:
+        print('O Número NÃO é Primo')
+    elif num % 5 == 0:
+        print('O Número NÃO é Primo')
+    elif num % 6 == 0:
+        print('O Número NÃO é Primo')
+    elif num % 7 == 0:
+        print('O Número NÃO é Primo')
+    elif num % 8 == 0:
+        print('O Número NÃO é Primo')
+    elif num % 9 == 0:
+        print('O Número NÃO é Primo')
+    else:
+        print('O Número é Primo')
+        
+ #---------------------------------      
+
+print('Identificar Número Primo')
+print('----------')
+
+num = int(input('Digite um Número de 1 a 100: '))
+print('----------')
+
+num_primo()
